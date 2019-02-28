00904c70ec6bb85aa22a384ce9896a889449ae56
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-02-28 19:08:00 +0600

    template settings

--- btre/settings.py
+++ btre/settings.py
@@ -54,8 +54,9 @@ ROOT_URLCONF = 'btre.urls'
 
 TEMPLATES = [
     {
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
