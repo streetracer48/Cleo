82a2fba1838a9b6e14d39198863b5c6e129fc776
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-03-02 23:07:02 +0600

    git partial added

--- btre/settings.py
+++ btre/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER':'postgres',
+        'PASSWORD':'12345',
+        'HOST':'localhost'
     }
 }
 
@@ -119,4 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT= os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
