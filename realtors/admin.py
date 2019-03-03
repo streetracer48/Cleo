c4b6cbe267bb15bb2797881489ece60506ec8689
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-03-03 20:51:11 +0600

    modified admin data showing

--- realtors/admin.py
+++ realtors/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from .models import Realtor
 
 # Register your models here.
-admin.site.register(Realtor)
+
+#Customize admin data showing
+class RealtorAdmin(admin.ModelAdmin):
+    list_display=('id', 'name', 'email', 'hire_date')
+    list_display_links=('id', 'name')
+    search_fields=('email',)
+    list_per_page=25
+
+admin.site.register(Realtor, RealtorAdmin)
