a101a5066e4fb4e21e560f3439efca662370a2ab
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-03-03 17:04:30 +0600

    realtor model create

--- listings/models.py
+++ listings/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
 # Create your models here.
 
@@ -6,15 +8,15 @@ class Listing(models.Model):
     realtor=models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
     title= models.CharField(max_length=200)
     address = models.CharField(max_length=200)
-    city = model.CharField(max_length=100)
+    city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=100)
     description= models.TextField(blank= True)
-    price= models.IntgerField()
-    bedrooms= models.IntgerField()
+    price= models.IntegerField()
+    bedrooms= models.IntegerField()
     bathrooms= models.DecimalField(max_digits=2, decimal_places=1)
-    garage= models.IntergerField(default=0)
-    sqft= models.IntgerField()
+    garage= models.IntegerField(default=0)
+    sqft= models.IntegerField()
     lot_size= models.DecimalField(max_digits=5, decimal_place=1)
     photo_main=models.ImageField(upload_to='photos/%Y/%m/%d/')
     photo_1=models.ImageField(upload_to='photos/%Y/%m/%d/',blank=True)
