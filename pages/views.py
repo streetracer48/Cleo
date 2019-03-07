caeefc09658223b6419f3f12942ff3a664b57432
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-03-08 01:00:36 +0600

    house list show in index pages dynamicly

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:6]
+
+    context = {
+        'listings':listings
+     }
+    return render(request, 'pages/index.html', context)
 
 def about(request):
     return render(request, 'pages/about.html')
