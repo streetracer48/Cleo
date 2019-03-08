5d446961e1066a5c64d49bbebb0663ea324e2691
mdmirajulkarim <mdmirajulkarim@gmail.com>
2019-03-08 21:52:41 +0600

    making independent objects file for iterate price value

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import state_choices, bedroom_choices, price_choices
 
 # Create your views here.
 
@@ -9,7 +10,10 @@ def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:6]
 
     context = {
-        'listings':listings
+        'listings':listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices':price_choices
      }
     return render(request, 'pages/index.html', context)
 
